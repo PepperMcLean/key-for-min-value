105e5a46b111dadd468d93915f2765ceabb638a6
PepperMcLean <jackmcl206@gmail.com>
2021-02-13 18:36:06 -0600

    commit tree 09a507940470e8bdd9a64027271a4836a69c3395

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,16 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+    number = Float::INFINITY
+    item = ""
+    if name_hash == {}
+        return nil
+    end
+    name_hash.each do |thing, amount|
+        if amount < number
+            number = amount
+            item = thing
+        end
+    end
+    return item
 end
